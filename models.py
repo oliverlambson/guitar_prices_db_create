48dc28915daa4db56588357cf3a6a559f5d57307
Oliver Lambson <oliverlambson@gmail.com>
2018-11-09 19:12:47 +0200

    Added __str__ methods to models

--- models.py
+++ models.py
@@ -11,20 +11,32 @@ class BaseModel(pw.Model):
 class Brand(BaseModel):
     name = pw.CharField(unique=True)
 
+    def __str__(self):
+        return f'Brand: {self.name}'
+
 
 class SubBrand(BaseModel):
     name = pw.CharField()
     brand = pw.ForeignKeyField(Brand, backref='sub_brands')
+
+    def __str__(self):
+        return f'SubBrand: {self.name} - of Brand: {self.brand.name}'
 
 
 class Range(BaseModel):
     name = pw.CharField()
     brand = pw.ForeignKeyField(Brand, backref='ranges')
 
+    def __str__(self):
+        return f'Range: {self.name} - of Brand: {self.brand.name}'
+
 
 class Model(BaseModel):
     name = pw.CharField()
     brand = pw.ForeignKeyField(Brand, backref='models')
+
+    def __str__(self):
+        return f'Model: {self.name} - of Brand: {self.brand.name}'
 
 
 class Guitar(BaseModel):
@@ -35,6 +47,9 @@ class Guitar(BaseModel):
     sub_brand = pw.ForeignKeyField(SubBrand, backref='sub_brands')
     range_name = pw.ForeignKeyField(Range, backref='ranges')
     model = pw.ForeignKeyField(Model, backref='models')
+
+    def __str__(self):
+        return f'Guitar: {self.brand.name} {self.sub_brand.name} {self.range_name.name} {self.model.name} {self.variant} {self.year} ${self.price}'
 
 
 def create_db_tables():
